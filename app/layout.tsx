61de57181ccfcd58f8c62b1cd6981d9c34144c25
Shahid Ali Sethi <your_email@example.com>
2026-01-19 15:13:32 +0500

    feat: added professional sonner toasts for copy action

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,18 +31,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   );
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
-}
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
